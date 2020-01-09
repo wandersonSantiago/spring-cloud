15ec4303079c28c5729e68b4155c368db19c10df
wandersonSantiago <wandersonsantiago86@gmail.com>
2020-01-09 18:00:00 -0300

    commit inicial api auth

---
+++ auth/src/main/java/com/microservice/auth/AuthApplication.java
@@ -0,0 +1,27 @@
+package com.microservice.auth;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
+@RestController
+public class AuthApplication {
+
+	@GetMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
